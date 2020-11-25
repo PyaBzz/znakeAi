48f8ca0176056c2aa3b874b07b28bfa369aab65c
pyabzz <>
2020-11-25 21:30:07 +0000

    Update visualiser

--- script/52-visualiser.js
+++ script/52-visualiser.js
@@ -1,17 +1,19 @@
 Visualiser = function (game) {
+    this.game = game;
 }
 
-Visualiser.prototype.visualise = function (matrix) {
+Visualiser.prototype.visualiseGrid = function () {
+    let inputMatrix = this.getGridMatrix();
     tfvis.visor();
-    const inputObj = { values: matrix };
+    const inputObj = { values: inputMatrix };
     const surface = { name: "dasoo name", tab: "dasoo tab" }
     tfvis.render.heatmap(surface, inputObj);
 }
 
-Visualiser.prototype.getInputMatrix = function () {
+Visualiser.prototype.getGridMatrix = function () {
     let gridCells = this.game.grid.cells;
     let values = [];
     for (let row of gridCells)
-        values.push(row.map(this.getCellValue));
+        values.push(row.map(c => c.value));
     return values;
 }
