e6b96a2da20f61aa2c98d56e8dea41f5dcc62fb0
pyabzz <>
2020-11-28 16:46:40 +0000

    Use latest bazJS

--- script/04-worm.js
+++ script/04-worm.js
@@ -86,7 +86,7 @@ Worm.prototype.die = function (nextHeadCell) {
 
 Worm.prototype.getDirectionFromOutput = function (tensor) {
     let array = tensor.arraySync()[0];
-    let indexOfMax = array.getIndexOfMax();
+    let indexOfMax = array.getMax()[0];
     return indexOfMax + 1;  // because directions start from 1
 }
 
